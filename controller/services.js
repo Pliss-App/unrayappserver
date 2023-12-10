3b7a9e6c730e6640dc96ff3807b2fee46fd58a9a
Pliss-App <pliissapp5@gmail.com>
2023-12-10 11:52:07 -0600

    make it better

--- controller/services.js
+++ controller/services.js
@@ -23,9 +23,8 @@ const getDriver = () => { //getByEmail
 const getDriverService = (id, lat, lng) => { //getByEmail
     return new Promise((resolve, reject) => {
         connection.query(
-           `SELECT @punto := 'POINT(',?,' ',?,')';
-           SELECT ( u.id,  u.idService, u.uid, u.name, lo.lat, lo.lng, u.id_status, u.idStatus_travel, (6371 * acos(cos(radians(X(POINTFROMTEXT(@punto))) ) * cos( radians( X(lo.location) ) ) * cos( radians( Y(lo.location) ) radians(Y(POINTFROMTEXT(@punto))) ) + sin ( radians(X(POINTFROMTEXT(@punto))) ) * sin( radians( X(lo.location))))) AS distance_km ) INTO result FROM user u INNER JOIN location lo ON u.uid = lo.uid where u.id_type= 2 AND u.id_status=1 AND u.idStatus_travel= 0 AND u.idService=? HAVING distance_km < 10 ORDER BY distance_km;
-           `,[lat, lng, id], (err, rows) => {
+           `SELECT @punto := 'POINT(',14.524522609950612, ' ', -91.68588609446668,')';
+           SELECT u.id,  u.idService, u.uid, u.name, lo.lat, lo.lng, u.id_status, u.idStatus_travel, (6371 * acos(cos (radians(X(POINTFROMTEXT(@punto))) ) * cos( radians( X(lo.location) ) ) * cos( radians( Y(lo.location) ) -radians(Y(POINTFROMTEXT(@punto))) )+ sin ( radians(X(POINTFROMTEXT(@punto))) )* sin( radians( X(lo.location) ) ))) AS distance_km FROM user u INNER JOIN location lo ON u.uid = lo.uid where u.id_type= 2 AND u.id_status=1 AND u.idStatus_travel= 0`,[lat, lng, id], (err, rows) => {
                 if (err) reject(err)
                 resolve(rows)
             });
