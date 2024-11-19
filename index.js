0bdb35ed95d7e54a846af8dd4ad84c16abc9c08f
institutosanluis <insitutosanluis@gmail.com>
2024-11-19 17:55:18 -0600

    first commit

--- index.js
+++ index.js
@@ -56,5 +56,5 @@ const keepServerAlive = () => {
 //const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
-    keepServerAlive();
+  //  keepServerAlive();
 });
