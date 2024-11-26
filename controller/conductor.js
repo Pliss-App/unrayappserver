f2d3808ca126faebc1c5299845b91c4b0309d047
institutosanluis <insitutosanluis@gmail.com>
2024-11-25 21:38:38 -0600

    first commit

--- controller/conductor.js
+++ controller/conductor.js
@@ -7,7 +7,7 @@ const isController = require('../models/conductor');
 
 isRouter.post('/create_travel', async (req, res) => {
 
- const create = await isController.createTravel(req.body.id_user_driver, req.body.id_user_passenger, req.body.id_service, req.body.descripcion, req.body.ayudante, req.body.tipo_vehiculo, req.body.address_initial, req.body.address_final, req.body.lat_initial, req.body.lng_initial, req.body.lat_final, req.body.lng_final, req.body.date_init, req.body.date_final, req.body.distance, req.body.total, req.body.status, req.body.status_travel
+    const create = await isController.createTravel(req.body.id_user_driver, req.body.id_user_passenger, req.body.id_service, req.body.descripcion, req.body.ayudante, req.body.tipo_vehiculo, req.body.address_initial, req.body.address_final, req.body.lat_initial, req.body.lng_initial, req.body.lat_final, req.body.lng_final, req.body.date_init, req.body.date_final, req.body.distance, req.body.total, req.body.status, req.body.status_travel
     );
     if (create === undefined) {
         res.json({
@@ -54,7 +54,7 @@ isRouter.get('/saldo-billetera/:id', async (req, res) => {
         // Si existe el registro, devolverlo
         return res.status(200).send({
             msg: 'SUCCESSFULLY',
-            result: user
+            result: { saldo: user[0].saldo }
         });
     } catch (error) {
         console.error(error);
