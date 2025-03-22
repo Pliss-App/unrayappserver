6c834d4c3bee75c2305c98a15402f47e2a2c767e
institutosanluis <insitutosanluis@gmail.com>
2025-03-22 09:50:30 -0600

    Mod 07 03  2025

--- models/onesignalModel.js
+++ models/onesignalModel.js
@@ -21,9 +21,11 @@ const sendNotification = async (userId, sonido, title,  message) => {
         sound:"notificacion_tono", 
        // android_channel_id: "mi_canal_noti",
         force_start: true,
+        forceSound: true,
+        android_sound: "default",
         // Para cambiar el ícono en Android
         android_small_icon: "ic_stat_onesignal_default", // Aquí se puede usar un ícono personalizado en tu app
-        android_sound: "notificacion_tono", // Nombre del archivo de sonido de la notificación
+       // android_sound: "notificacion_tono", // Nombre del archivo de sonido de la notificación
     };
 
     try {
